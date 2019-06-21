bae936fea59062e79d7703dc4cb2d01c6abe8951
senapatirupen <senapatirupen1986@gmail.com>
2019-06-21 11:54:29 +0530

    Signed-off-by: senapatirupen <senapatirupen1986@gmail.com>

--- demo/src/main/java/com/exspring/demo/web/controller/PollController.java
+++ demo/src/main/java/com/exspring/demo/web/controller/PollController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
-<<<<<<< HEAD
 import javax.validation.Valid;
 
 import org.springframework.http.HttpHeaders;
@@ -68,70 +67,7 @@ public class PollController {
 
 	@RequestMapping(value = "/polls", method = RequestMethod.POST)
 	public ResponseEntity<?> createPoll(@Valid @RequestBody Poll poll) {
-=======
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.exspring.demo.dto.PartyCount;
-import com.exspring.demo.dto.VoteResult;
-import com.exspring.demo.entity.Party;
-import com.exspring.demo.entity.Poll;
-import com.exspring.demo.entity.Vote;
-import com.exspring.demo.repository.PartyRepository;
-import com.exspring.demo.repository.PollRepository;
-import com.exspring.demo.repository.VoteRepository;
-import com.exspring.demo.web.exception.ResourceNotAvailableException;
-import com.exspring.demo.web.exception.ResourceNotFoundException;
-
-@RestController
-public class PollController {
-
-	@Inject
-	private PollRepository pollRepository;
-
-	@Inject
-	private PartyRepository partyRepository;
-
-	@Inject
-	private VoteRepository voteRepository;
-
-	@RequestMapping(value = "/polls", method = RequestMethod.GET)
-	public ResponseEntity<Iterable<Poll>> getAllPolls() {
-		Iterable<Poll> allPolls = pollRepository.findAll();
-		return new ResponseEntity<>(allPolls, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/parties", method = RequestMethod.GET)
-	public ResponseEntity<Iterable<Party>> getAllParty() {
-		Iterable<Party> allParties = partyRepository.findAll();
-		return new ResponseEntity<>(allParties, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/votes", method = RequestMethod.GET)
-	public ResponseEntity<Iterable<Vote>> getAllVotes() {
-		Iterable<Vote> allVotes = voteRepository.findAll();
-		return new ResponseEntity<>(allVotes, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/polls/{pollId}/votes", method = RequestMethod.GET)
-	public Iterable<Vote> getAllVotes(@PathVariable Long pollId) {
-		return voteRepository.findByPoll(pollId);
-	}
-
-	@RequestMapping(value = "/polls", method = RequestMethod.POST)
-	public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
->>>>>>> branch 'master' of https://github.com/senapatirupen/springall.git
 		poll = pollRepository.save(poll);
-
 		// Set the location header for the newly created resource
 		HttpHeaders responseHeaders = new HttpHeaders();
 		URI newPollUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(poll.getId())
